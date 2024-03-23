20c36c0bac8f78b6e9fdb5af3f00df38c66f2bb8
fastbricks <125431180+fastbricks@users.noreply.github.com>
2024-03-23 16:00:38 +0530

    Update page.js

--- src/app/(footer)/contact/page.js
+++ src/app/(footer)/contact/page.js
@@ -188,7 +188,7 @@ export default function Contact() {
                     type="submit"
                     className="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
                   >
-                    Submit
+                    Send Query
                   </button>
                 </form>
               ) : (
