d493fd1c878dab3035a46759e4894929a6d8eddd
fastbricks <125431180+fastbricks@users.noreply.github.com>
2024-03-24 10:03:24 +0530

    Update SearchPageMobileView.js

--- src/components/SearchBar/SearchPageMobileView.js
+++ src/components/SearchBar/SearchPageMobileView.js
@@ -1,38 +1,29 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Dialog from "@mui/material/Dialog";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemButton from "@mui/material/ListItemButton";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
-import Slide from "@mui/material/Slide";
-import SearchBar from "../Navbar/SearchBar";
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
+import List from "@mui/material/List";
+import Slide from "@mui/material/Slide";
 import styles from "@/components/Navbar/mobileNav.module.css";
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
+const Transition = (props) => {
+  return <Slide direction="up" {...props} />;
+};
 
 export default function SearchPageMobileView() {
-  const [open, setOpen] = React.useState(true);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  const [open, setOpen] = useState(true);
+  const router = useRouter();
 
   const handleClose = () => {
-    setOpen(true);
+    router.push("/"); // Navigate to the home screen
   };
 
   return (
-    // <React.Fragment>
     <Dialog fullScreen open={open} TransitionComponent={Transition}>
       <AppBar sx={{ position: "relative", backgroundColor: "red" }}>
         <Toolbar>
@@ -47,13 +38,11 @@ export default function SearchPageMobileView() {
           <div className={styles.searchContainer}>
             <SearchIcon />
             <input type="text" placeholder="Search for" />
-
             <MicIcon />
           </div>
         </Toolbar>
       </AppBar>
       <List></List>
     </Dialog>
-    // </React.Fragment>
   );
 }
