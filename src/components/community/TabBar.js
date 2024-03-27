41e375f276d24857683471881c8281da0425e9c7
fastbricks <125431180+fastbricks@users.noreply.github.com>
2024-03-27 21:25:26 +0530

    Update TabBar.js

--- src/components/community/TabBar.js
+++ src/components/community/TabBar.js
@@ -32,7 +32,7 @@ export default function TabBar() {
           }}
         >
           <Tab
-            label="A"
+            label="All"
             sx={{
               textTransform: "capitalize",
 
