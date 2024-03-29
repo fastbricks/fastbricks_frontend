ee1852e2d4d9edcc76524f8917a76ba841463133
fastbricks <125431180+fastbricks@users.noreply.github.com>
2024-03-29 10:56:39 +0530

    Update page.js

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -2,8 +2,8 @@ import BottomNavBar from "@/components/BottomNavigation/BottomNavBar";
 
 export default function Account() {
   return (
-    <>
+    <main className="h-[100vh]">
       <BottomNavBar />
-    </>
+    </main>
   );
 }
