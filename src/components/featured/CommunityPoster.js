fd4c7d0802035219da0110a32fffb5b7d681232b
fastbricks <125431180+fastbricks@users.noreply.github.com>
2024-04-05 16:49:12 +0530

    Update CommunityPoster.js

--- src/components/featured/CommunityPoster.js
+++ src/components/featured/CommunityPoster.js
@@ -1,21 +1,34 @@
 "use client";
 import poster from "@/components/services/assets/community.png";
-import posterMobile from "@/components/services/assets/posterMobile.png";
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 
 function CommunityPoster() {
-  return (
-    <div>
-      <hr className="bg-gray-100 p-1 block sm:hidden" />
-      <Link href={"/community"}>
-        <div className=" p-2 hidden sm:block cursor-pointer mb-5">
-          <Image src={poster} className="rounded" />
-        </div>
-      </Link>
-    </div>
-  );
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = window.navigator.userAgent;
+    setIsMobile(
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        userAgent
+      )
+    );
+  }, []);
+  if (!isMobile) {
+    return (
+      <div className="">
+        <hr className="bg-gray-100 p-1 block sm:hidden" />
+        <Link href={"/community"}>
+          <div className=" p-2  cursor-pointer mb-5">
+            <Image src={poster} className="rounded" />
+          </div>
+        </Link>
+      </div>
+    );
+  }
+  return null;
 }
 
 export default CommunityPoster;
