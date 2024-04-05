88509a94a14bbc6813d67b502ced87343cb3f31e
fastbricks <125431180+fastbricks@users.noreply.github.com>
2024-04-05 16:42:13 +0530

    Update CommunityPoster.js

--- src/components/featured/CommunityPoster.js
+++ src/components/featured/CommunityPoster.js
@@ -10,13 +10,8 @@ function CommunityPoster() {
     <div>
       <hr className="bg-gray-100 p-1 block sm:hidden" />
       <Link href={"/community"}>
-        <div className="mt-5 p-2 hidden sm:block cursor-pointer mb-5">
+        <div className=" p-2 hidden sm:block cursor-pointer mb-5">
           <Image src={poster} className="rounded" />
-        </div>
-      </Link>
-      <Link href={"/community"}>
-        <div className="mt-5 p-5 block sm:hidden cursor-pointer mb-5 ">
-          <Image src={poster} className="rounded-lg" />
         </div>
       </Link>
     </div>
